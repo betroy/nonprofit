ef3effbac7005d373a3aaf6dc9e56c10361d9a03
Troy <chenlongfeiceo@gmail.com>
2021-04-06 21:26:51 +0800

    feat:update cloudbaserc

--- cloudbaserc.js
+++
@@ -1,5 +0,0 @@
-{
-  "TCB_SERVICE_DOMAIN": "nonprofit-8g11k5jj7aa730f7-1254641557.ap-shanghai.service.tcloudbase.com",
-  "TCB_ENV_ID": "nonprofit-8g11k5jj7aa730f7",
-  "TCB_REGION": "ap-shanghai"
-}
