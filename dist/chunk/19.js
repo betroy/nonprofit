190a8b5b812a910c0fb720613d3f766e76e20a8f
Troy <chenlongfeiceo@gmail.com>
2021-04-06 13:18:08 +0800

    feat:dist commit

---
+++ dist/chunk/19.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{"63":function(r,n,o){"use strict";o.r(n),o.d(n,"taro_camera_core",(function(){return c}));var e=o(30);const c=class{"constructor"(r){Object(e.g)(this,r)}"componentDidLoad"(){console.error("H5 暂不支持 Camera 组件！")}"render"(){return Object(e.e)(e.a,null)}}}}]);
